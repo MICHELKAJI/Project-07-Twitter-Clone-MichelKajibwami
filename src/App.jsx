9f3d086095f3bd2fdbe27c8827815e73315f4dd7
MICHELKAJI <121295124+MICHELKAJI@users.noreply.github.com>
2023-11-23 20:33:53 +0000

    add news

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ export default function App() {
       <h1>Hello word</h1>
       <h2>Hello word</h2>
       <h3>Hello word</h3>
+      <h4>Hello word</h4>
+      <h5>Hello word</h5>
+      <h6>Hello word</h6>
+
 
     </Layout>
   );
