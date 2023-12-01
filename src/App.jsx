dd6d871295ffcbd02819afd1e2a4b6d4626e673a
MICHELKAJI <121295124+MICHELKAJI@users.noreply.github.com>
2023-12-01 08:26:49 +0000

    add  new fils

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,7 @@ import('./style/App.css');
   function App() {
   return (
     <Layout>
-<<<<<<< HEAD
-      <Home />
-      <h1>Hello word</h1>
-      <h2>Hello word</h2>
-      <h3>Hello word</h3>
-      <h4>Hello word</h4>
-      <h5>Hello word</h5>
-      <h6>Hello word</h6>
-
-
-=======
-       <Home/>
->>>>>>> e5cff5b1a4109296a4587a29e85b7e0e2a6bcd38
+      <Home/>
     </Layout>
   );
 }
