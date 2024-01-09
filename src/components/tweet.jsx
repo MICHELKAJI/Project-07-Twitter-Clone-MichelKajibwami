34782bc0a07d9439167e6557de9e85ae7f2651d9
Michel <fistonkajibwami2@gmail.com>
2024-01-09 12:13:01 +0100

    creation object table of tweet

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import images from "../images/tweetimage.png";
+import images from "../images/Reply.svg";
+import iconLike from "../images/Like.svg";
+import iconRetweet from "../images/Retweet.svg";
+import iconDownload from "../images/Downlord.svg"
+
 
 function Tweets() {
     return (
@@ -15,87 +19,25 @@ function Tweets() {
 function Tweet({tweet}) {
     return (
         <div className="tweet">
-            <TweetProfil icon={tweet.postImage} />
+            <TweetProfil icon={tweet.profilTweet} />
 
             <div className="tweet-content">
                 <div className="tweet-body">
-                    <TweetTitle text={tweet.tweetTitle} icon="src/images/Group.svg" strong="Tukya@ 1h." />
+                    <TweetTitle text={tweet.tweetTitle} icon={tweet.iconTitle} strong={tweet.tweetpseudo}  />
                     <TweetText text={tweet.text} />
+                    <TweetImage images={tweet.postImage}/>
                 </div>
-                <TweetActions />
+                <div className="tweet-actions">
+            <TweetAction icon={images} value={tweet.comment} />
+            <TweetAction icon={iconRetweet} value={tweet.retweet} />
+            <TweetAction icon={iconLike} value={tweet.like} />
+            <TweetAction icon={iconDownload} />
+        </div>
             </div>
         </div>
 
     )
 }
-// function TweetOne (){
-//     return (
-
-//     )
-// }
-
-// function TweetTow(){
-//     return (
-//         <div className="tweet">
-//                 <TweetProfil icon="src\images\twetIcone.png"/>
-//                <div className="tweet-content">
-//                <div className="tweet-body">
-//                  <TweetTitle text="CNN" icon="src/images/Group.svg" strong="Tukya@ 1h."/>
-//                  <TweetText text= "Bonjour juste vous informer que suis pas d'humeur"/>
-//                  <TweetImage/>
-//              </div>
-//              <TweetActions/>
-//                </div>
-//         </div>
-
-//     )
-// };
-
-// function TweetThree (){
-//     return (
-//         <div className="tweet">
-//         <TweetProfil icon="src/images/twitter.svg"/>
-
-//        <div className="tweet-content">
-//        <div className="tweet-body">
-//          <TweetTitle text="CNN" icon="src/images/Group.svg" strong="Tukya@ 1h."/>
-//          <TweetText text= "Bonjour suis développeur fullstack pour vos projets de digitalisation n'hésité pas à nous consulter"/>
-//      </div>
-//      <TweetActions/>
-//        </div>
-// </div> )
-// }
-
-// function TweetFor (){
-//     return (
-//         <div className="tweet">
-//         <TweetProfil icon="src/images/twitter.svg"/>
-
-//        <div className="tweet-content">
-//        <div className="tweet-body">
-//          <TweetTitle text="CNN" icon="src/images/Group.svg" strong="Tukya@ 1h."/>
-//          <TweetText text= ""/>
-//      </div>
-//      <TweetActions/>
-//        </div>
-// </div>)}
-
-// function TweetFive (){
-//     return (
-//             <div className="tweet">
-//                 <TweetProfil icon="src/images/twitter.svg"/>
-
-//                <div className="tweet-content">
-//                <div className="tweet-body">
-//                  <TweetTitle text="CNN" icon="src/images/Group.svg" strong="Tukya@ 1h."/>
-//                  <TweetText text= "Bonjour suis développeur fullstack pour vos projets de digitalisation n'hésité pas à nous consulter"/>
-//              </div>
-//              <TweetActions/>
-//                </div>
-//         </div>
-//         )
-// }
-
 function TweetTitle({ text, strong, icon }) {
     return <div className="tweet-title">
         <h2 className="tweet-title-author">{text}</h2>
@@ -116,7 +58,7 @@ function TweetText({ text }) {
     )
 }
 
-function TweetImage() {
+function TweetImage({images}) {
     return (
         <div className="tweet-image">
             <img src={images} alt="imageTweet" />
@@ -132,16 +74,7 @@ function TweetAction({ icon, value }) {
         </div>
     )
 }
-function TweetActions() {
-    return (
-        <div className="tweet-actions">
-            <TweetAction icon="src/images/Reply.svg" value="19" />
-            <TweetAction icon="src/images/Retweet.svg" value="120" />
-            <TweetAction icon="src/images/Like.svg" value="190" />
-            <TweetAction icon="src/images/Downlord.svg" />
-        </div>
-    )
-}
+
 function TweetProfil({ icon }) {
     return (
         <div className="tweet-avatar">
@@ -154,16 +87,34 @@ const datas = [
     {
         id: 1,
         autheur: "Michelkaji",
-        profil: "src\images\profile.png",
+        profil: "src/images/profile.png",
+        profilTweet:"src/images/tweetprofile.png",
         text: "Bonjour suis développeur fullstack pour vos projets de digitalisation n'hésité pas à nous consulter",
-        postImage: "src\images\tweetimage.png",
+        postImage: "src/images/tweetimage.png",
         tweetTitle: "CNN",
-        tweetpseudo: "@CNN"
+        tweetpseudo: "@MichelKaji 1h",
+        iconTitle : "src/images/Group.svg",
+        comment : 49,
+        like : 100,
+        retweet : 2
+    },
+    {
+        
+            id: 2,
+            autheur: "Tukya",
+            profil: "src/images/profile.png",
+            profilTweet:"src/images/twetIcone.png",
+            text: "Bonjour suis développeur fullstack pour vos projets de digitalisation n'hésité pas à nous consulter react",
+            postImage: "src/images/tweetimage.png",
+            tweetTitle: "New York Time",
+            tweetpseudo: "@Tukya h1",
+            iconTitle : "src/images/Group.svg",
+            comment : 1000,
+            like : 100,
+            retweet : 2
+        
     }
 ]
-
-
-
 
 export default Tweets;
 
