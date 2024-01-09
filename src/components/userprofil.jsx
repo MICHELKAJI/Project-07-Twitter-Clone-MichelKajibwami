80c97ac20493d4ab43f53ceb3e58efade35ba790
MICHELKAJI <121295124+MICHELKAJI@users.noreply.github.com>
2024-01-09 15:30:46 +0000

    add data

--- src/components/userprofil.jsx
+++ src/components/userprofil.jsx
@@ -1,22 +1,23 @@
+import ProfilAvatar from "./profilAvatar";
 import Button from "./buttonTwitter";
 import HeaderProfil from "./headerProfil";
 import NavBarre from "./navBarre";
 import TweetForm from "./tweet";
 
-function UserProfile(){
-    return(
-        <div className="styleUserProfil">
-            <div className="iconProfil">
-                <div><ProfilAvatar imag='src\images\profile.png' style='avatares'/></div>
-                <div><Button style= "buttone">Edit Profil</Button></div>
-            </div>
-            <div className="" >
-                <HeaderProfil name="MichelKaji" soustitle="@MichelKaji"/>
-            </div>
-            <h3 className="titlesuser"><span className="tweet-text">160</span>Abonement</h3>
-            <NavBarre/>
-            <TweetForm/>
-        </div>
-    )
+function UserProfile() {
+  return (
+    <div className="styleUserProfil">
+      <div className="iconProfil">
+        <div><ProfilAvatar imag='src\images\profile.png' style='avatares' /></div>
+        <div><Button style="buttone">Edit Profil</Button></div>
+      </div>
+      <div className="" >
+        <HeaderProfil name="MichelKaji" soustitle="@MichelKaji" />
+      </div>
+      <h3 className="titlesuser"><span className="tweet-text">160</span>Abonement</h3>
+      <NavBarre />
+      <TweetForm />
+    </div>
+  )
 }
 export default UserProfile;
