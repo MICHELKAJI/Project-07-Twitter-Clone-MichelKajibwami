1dd2ebc2fdb8435dee94da3a158c55dc5a95146e
Michel <fistonkajibwami2@gmail.com>
2024-01-10 11:25:59 +0100

    hover

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -5,6 +5,29 @@ import iconRetweet from "../images/Retweet.svg";
 import iconDownload from "../images/Downlord.svg"
 
 
+function Tweet({ tweet }) {
+    return (
+        <div className="tweet">
+            <TweetProfil icon={tweet.profilTweet} />
+
+            <div className="tweet-content">
+                <div className="tweet-body">
+                    <TweetTitle text={tweet.tweetTitle} icon={tweet.iconTitle} strong={tweet.tweetpseudo} />
+                    <TweetText text={tweet.text} />
+                    <TweetImage images={tweet.postImage} />
+                </div>
+                <div className="tweet-actions ">
+                    <TweetAction icon={images} value={tweet.comment} />
+                    <TweetAction icon={iconRetweet} value={tweet.retweet} />
+                    <TweetAction icon={iconLike} value={tweet.like} />
+                    <TweetAction icon={iconDownload} />
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
 function Tweets() {
     return (
         <div>
@@ -17,28 +40,6 @@ function Tweets() {
     )
 }
 
-function Tweet({tweet}) {
-    return (
-        <div className="tweet">
-            <TweetProfil icon={tweet.profilTweet} />
-
-            <div className="tweet-content">
-                <div className="tweet-body">
-                    <TweetTitle text={tweet.tweetTitle} icon={tweet.iconTitle} strong={tweet.tweetpseudo}  />
-                    <TweetText text={tweet.text} />
-                    <TweetImage images={tweet.postImage}/>
-                </div>
-                <div className="tweet-actions">
-            <TweetAction icon={images} value={tweet.comment} />
-            <TweetAction icon={iconRetweet} value={tweet.retweet} />
-            <TweetAction icon={iconLike} value={tweet.like} />
-            <TweetAction icon={iconDownload} />
-        </div>
-            </div>
-        </div>
-
-    )
-}
 
 function TweetTitle({ text, strong, icon }) {
     return <div className="tweet-title">
@@ -57,9 +58,10 @@ function TweetText({ text }) {
             </p>
 
         </div>
-    )}
+    )
+}
 
-function TweetImage({images}) {
+function TweetImage({ images }) {
     return (
         <div className="tweet-image">
             <img src={images} alt="imageTweet" />
@@ -70,7 +72,7 @@ function TweetImage({images}) {
 
 function TweetAction({ icon, value }) {
     return (
-        <div className="tweet-action">
+        <div className="tweet-action Vector">
             <div><img src={icon} alt="Tweet" /></div> <span className="tweet-title-details">{value}</span>
         </div>
     )
@@ -89,31 +91,31 @@ const datas = [
         id: 1,
         autheur: "Michelkaji",
         profil: "src/images/profile.png",
-        profilTweet:"src/images/tweetprofile.png",
+        profilTweet: "src/images/tweetprofile.png",
         text: "Bonjour suis développeur fullstack pour vos projets de digitalisation n'hésité pas à nous consulter",
         postImage: "src/images/tweetimage.png",
         tweetTitle: "CNN",
         tweetpseudo: "@MichelKaji 1h",
-        iconTitle : "src/images/Group.svg",
-        comment : 49,
-        like : 100,
-        retweet : 2
+        iconTitle: "src/images/Group.svg",
+        comment: 49,
+        like: 100,
+        retweet: 2
     },
     {
-        
-            id: 2,
-            autheur: "Tukya",
-            profil: "src/images/profile.png",
-            profilTweet:"src/images/twetIcone.png",
-            text: "Bonjour suis développeur fullstack pour vos projets de digitalisation n'hésité pas à nous consulter react",
-            postImage: "src/images/tweetimage.png",
-            tweetTitle: "New York Time",
-            tweetpseudo: "@Tukya h1",
-            iconTitle : "src/images/Group.svg",
-            comment : 1000,
-            like : 100,
-            retweet : 2
-        
+
+        id: 2,
+        autheur: "Tukya",
+        profil: "src/images/profile.png",
+        profilTweet: "src/images/twetIcone.png",
+        text: "Bonjour suis développeur fullstack pour vos projets de digitalisation n'hésité pas à nous consulter react",
+        postImage: "src/images/tweetimage.png",
+        tweetTitle: "New York Time",
+        tweetpseudo: "@Tukya h1",
+        iconTitle: "src/images/Group.svg",
+        comment: 1000,
+        like: 100,
+        retweet: 2
+
     }
 ]
 
